04be77a1f08635d61e77fc7c5f1e7453b4e577fe
mcthouacbb <mcthouacbb@gmail.com>
2026-05-19 16:42:03 -0400

    Following a tutorial xd

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,36 @@ use eframe::egui;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
+    let result = eframe::run_native(
         "Graphing Calculator",
         native_options,
-        Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
+        Box::new(|_cc| Ok(Box::new(MyEguiApp::default()))),
     );
-    println!("Hello, world!");
-}
 
-#[derive(Default)]
-struct MyEguiApp {}
-
-impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_global_style.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
+    if let Err(err) = result {
+        eprintln!("Failed to start graphing calculator: {}", err);
     }
 }
 
+#[derive(Default)]
+struct MyEguiApp {
+    label: String,
+    value: f32,
+}
+
 impl eframe::App for MyEguiApp {
-    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show_inside(ui, |ui| {
             ui.heading("Hello World!");
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut self.label);
+            });
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                self.value += 1.0;
+            }
+            ui.label(format!("Hello '{}', value: {}", self.label, self.value));
         });
     }
 }

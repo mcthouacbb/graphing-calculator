66501bd73cb9b80cd88208b7f1d610bbfe1c46ce
mcthouacbb <mcthouacbb@gmail.com>
2026-05-19 16:15:55 -0400

    Hello World egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use eframe::egui;
+
 fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Graphing Calculator",
+        native_options,
+        Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
+    );
     println!("Hello, world!");
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_global_style.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
 }

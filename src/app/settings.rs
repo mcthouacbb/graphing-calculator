7c84b943042ea0ebd142ca9796f88cf98c839195
mcthouacbb <mcthouacbb@gmail.com>
2026-05-30 16:12:29 -0400

    Add axis settings

--- src/app/settings.rs
+++ src/app/settings.rs
@@ -2,12 +2,31 @@ use crate::app::{camera::Camera, widgets::float_input};
 
 use eframe::egui;
 
+struct AxisSettings {
+    render: bool,
+    render_ticks: bool,
+    render_grid: bool,
+}
+
+impl Default for AxisSettings {
+    fn default() -> Self {
+        Self {
+            render: true,
+            render_ticks: true,
+            render_grid: true,
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct Settings {
     camera_left: String,
     camera_right: String,
     camera_bottom: String,
     camera_top: String,
+
+    x_axis: AxisSettings,
+    y_axis: AxisSettings,
 }
 
 impl Settings {
@@ -27,6 +46,16 @@ impl Settings {
                 cols[1].label("Top: ");
                 float_input(&mut cols[1], &mut self.camera_top, &mut camera.top);
             });
+
+            Self::axis_settings(ui, &mut self.x_axis, "x");
+            Self::axis_settings(ui, &mut self.y_axis, "y");
         });
+    }
+
+    fn axis_settings(ui: &mut egui::Ui, axis: &mut AxisSettings, name: &str) {
+        ui.label(format!("{} axis", name));
+        ui.checkbox(&mut axis.render, "Show axis");
+        ui.checkbox(&mut axis.render_ticks, "Show labels");
+        ui.checkbox(&mut axis.render_grid, "Show grid lines");
     }
 }
